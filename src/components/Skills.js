4a5fcd471acf6e29fc78e11837f32318b93d9799
Rushanksavant <rushanksavant34@gmail.com>
2022-05-12 14:04:29 +0530

    revised padding

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,8 +16,8 @@ import mytril from "../imgs/mythril.PNG"
 export default function Skills() {
     return (
         <div name='skills' className='w-full h-screen overflow-y-auto bg-neutral-900 border-b-[1px] border-gray-700'>
-            <div className='flex flex-col pt-[50px] lg:pl-[150px] pr-[50px]'>
-                <span className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#ccd6f6] ml-2 md:mb-[50px] mb-[20px] border-b-[3px] md:border-b-[5px] border-pink-500">
+            <div className='flex flex-col pt-[40px] lg:pl-[150px] pr-[50px]'>
+                <span className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#ccd6f6] ml-2 md:mb-[30px] mb-[10px] border-b-[3px] md:border-b-[5px] border-pink-500">
                     Ask me about</span>
                 <div className="grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 mb-[50px] sm:mb-[50px] px-[10px] gap-4">
                     <div className="text-[#ccd6f6] font-semibold mx-auto bg-neutral-800 hover:scale-110 hover:bg-neutral-700 duration-300 p-3 w-[100px] md:w-[130px] xl:w-[180px] flex flex-col shadow-lg shadow-neutral-600">
@@ -67,7 +67,7 @@ export default function Skills() {
                 </div>
 
 
-                <span className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#ccd6f6] ml-2 md:mb-[50px] mb-[20px] border-b-[3px] md:border-b-[5px] border-pink-500">
+                <span className="text-3xl md:text-5xl lg:text-6xl font-bold text-[#ccd6f6] ml-2 md:mb-[30px] mb-[10px] border-b-[3px] md:border-b-[5px] border-pink-500">
                     Exploring {/*<span className="text-sm sm:text-xl">(before June 2022)</span>*/}</span>
                 <div className="grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 pb-[15px] px-[10px] gap-4">
                     <div className="text-[#ccd6f6] font-semibold mx-auto bg-neutral-800 hover:scale-110 hover:bg-neutral-700 duration-300 p-3 w-[100px] md:w-[130px] xl:w-[180px] flex flex-col shadow-lg shadow-neutral-600">
